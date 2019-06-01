9a01b1e64327110b66a4d01aa7d1a1a421e9bc3c
kks1234 <945366712@qq.com>
2019-06-02 00:35:40 +0800

    inet函数族的使用

---
+++ test56/test_address.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+int main()
+{
+    char         buffer[32];
+    int          ret = 0;
+    int          host = 0;
+    int          network = 0;
+    unsigned int address = 0;
+    struct in_addr in;
+    in.s_addr = 0;
+
+    /* 输入一个以.为分隔符形式的IP地址 */
+    printf("Please input your ip address\n");
+        fgets(buffer,31,stdin);
+    buffer[31] = '\0';
+
+    /* 示例使用inet_aton()函数 */
+    if( (ret = inet_aton(buffer, &in)) == 0 ) {
+        printf("inet_aton: \t invalid address \t\n");
+    } else {
+        printf("inet_aton: \t0x%x\n",in.s_addr);
+    }
+
+    /* 示例inet_addr函数 */
+    if( (address = inet_addr(buffer)) == INADDR_NONE ) {
+        printf("inet_addr: \t invalid address \t\n");
+    } else {
+        printf("inet_addr: \t0x%x\n",address);
+    }
+
+    /* 示例使用inet_network函数 */
+    if( (address = inet_network(buffer)) == -1 ) {
+        printf("inet_network:  \t invalid address \t\n");
+    } else {
+        printf("inet_network: \t0x%x\n",address);
+    }
+
+    /* 示例inet_ntoa函数 */
+    if( inet_ntoa(in) == NULL ) {
+        printf("inet_ntoa:  \t invalid address \t\n");
+    } else {
+        printf("inet_ntoa:  \t%s\n",inet_ntoa(in));
+    }
+
+    /* 示例inet_lnaof和inet_netof函数 */
+    host = inet_lnaof(in);
+    network = inet_netof(in);
+
+    printf("inet_lnaof: \t0x%x\n",host);
+    printf("inet_netof: \t0x%x\n",network);
+
+    in = inet_makeaddr(network, host);
+    printf("inet_makeaddr： 0x%x\n",in.s_addr);
+
+    return 0;
+}
